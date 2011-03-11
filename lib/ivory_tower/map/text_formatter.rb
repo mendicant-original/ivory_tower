68bd8caaba4a68ebd04de5ec5b97930b1463e5b7
Gregory Brown <gregory.t.brown@gmail.com>
2011-03-11 09:32:01 -0500

    Note about why the heck I'm use collect()

--- lib/ivory_tower/map/text_formatter.rb
+++ lib/ivory_tower/map/text_formatter.rb
@@ -8,6 +8,9 @@ module IvoryTower
       attr_reader :map
 
       def to_s
+        # NOTE TO STUDENTS: using collect instead of map here just 
+        # to avoid term overloading. Enumerable#map is typically the 
+        # preferred alias.
         map.tiles.collect do |row|
           row.collect do |tile|
             case tile
